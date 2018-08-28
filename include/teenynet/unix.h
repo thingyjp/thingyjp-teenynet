25e48cf30169287e6b2e1785c46999b326b1b8ba
Daniel Palmer <daniel@0x0f.com>
2018-08-29 00:20:35 +0900

    Clean up unix domain socket service create. Add unix domain socket service destroy.

--- include/teenynet/unix.h
+++ include/teenynet/unix.h
@@ -7,7 +7,9 @@ typedef gboolean (*incomingcallback)(GSocketService *service,
 		GSocketConnection *connection, GObject *source_object,
 		gpointer user_data);
 
-GSocketService* unix_createsocketlistener(const gchar* path,
+GSocketService* unix_socketservice_create(const gchar* path,
 		incomingcallback callback, gpointer user_data);
+
+void unix_socketservice_destroy(GSocketService* service, const gchar* path);
 
 #endif /* INCLUDE_TEENYNET_UNIX_H_ */
--- unix.c
+++ unix.c
@@ -1,7 +1,7 @@
 #include <gio/gunixsocketaddress.h>
 #include "include/teenynet/unix.h"
 
-GSocketService* unix_createsocketlistener(const gchar* path,
+GSocketService* unix_socketservice_create(const gchar* path,
 		incomingcallback callback, gpointer user_data) {
 	GSocketAddress* socketaddress = g_unix_socket_address_new(path);
 	GSocketService* socketservice = g_socket_service_new();
@@ -11,11 +11,24 @@ GSocketService* unix_createsocketlistener(const gchar* path,
 			socketaddress, G_SOCKET_TYPE_STREAM, G_SOCKET_PROTOCOL_DEFAULT,
 			NULL, NULL, &err)) {
 		g_message("failed to add listener; %s", err->message);
+		g_error_free(err);
+		goto err_addlistener;
 	}
-	g_error_free(err);
 
 	g_signal_connect(socketservice, "incoming", G_CALLBACK(callback),
 			user_data);
 
 	return socketservice;
+
+	err_addlistener: //
+	g_object_unref(socketservice);
+	return NULL;
+}
+
+void unix_socketservice_destroy(GSocketService* service, const gchar* path) {
+	g_socket_service_stop(service);
+	g_object_unref(service);
+	GFile* sockfile = g_file_new_for_path(path);
+	g_file_delete(sockfile, NULL, NULL);
+	g_object_unref(sockfile);
 }
